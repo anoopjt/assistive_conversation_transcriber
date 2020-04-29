096a48dd8f939441baab9292c24822414c61cd31
Preetham Goud <preethamsureshgoud@gmail.com>
2020-04-29 07:07:45 +0530

    Initial

---
+++ frontend/lib/widgets/group_chats.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GroupChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Center(
+        child: Text("Group chats"),
+      ),
+    );
+  }
+}
---
+++ frontend/lib/widgets/notes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Notes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Center(
+        child: Text("NOTES"),
+      ),
+    );
+  }
+}
