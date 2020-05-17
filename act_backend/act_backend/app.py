503c6fad750772afdae27e9a2ac4658afcb522ba
Fahim <faztp12@gmail.com>
2020-05-17 07:56:40 +0530

    chore: Added Gunicorn

--- act_backend/act_backend/app.py
+++ act_backend/act_backend/app.py
@@ -2,24 +2,15 @@ from flask import Flask, session
 from flask_session import Session
 from flask_socketio import SocketIO
 
+app = Flask(__name__)
+Session(app)
+socketio = SocketIO(app)
 
-def create_app():
-  app = Flask(__name__)
-  Session(app)
-  socketio = SocketIO(app)
+from .controllers.auth import init_app as init_auth_routes
+init_auth_routes(app)
 
-  from .controllers.auth import init_app as init_auth_routes
-  init_auth_routes(app)
-
-  from .controllers.transcription import init_app as init_trans_routes
-  init_trans_routes(app, session, socketio)
-
-  return {
-    "app": app,
-    "socketio": socketio
-  }
-
+from .controllers.transcription import init_app as init_trans_routes
+init_trans_routes(app, session, socketio)
 
 def start_dev_server():
-  app = create_app()
-  app["socketio"].run(app["app"], port=3000, host="0.0.0.0", debug=True)
+  socketio.run(app, port=3000, host="0.0.0.0", debug=True)
