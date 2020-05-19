b222b4ac96e8d689e14348da73848d968e9246df
Fahim <faztp12@gmail.com>
2020-05-19 07:54:39 +0530

    env: DEV_PORT

--- act_backend/act_backend/app.py
+++ act_backend/act_backend/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, session
 from flask_session import Session
 from flask_socketio import SocketIO
+import os
+dev_port = (os.environ['DEV_PORT']) or 3000
+if isinstance(dev_port, str):
+  dev_port = int(dev_port)
 
 app = Flask(__name__)
 Session(app)
@@ -13,4 +17,4 @@ from .controllers.transcription import init_app as init_trans_routes
 init_trans_routes(app, session, socketio)
 
 def start_dev_server():
-  socketio.run(app, port=3000, host="0.0.0.0", debug=True)
+  socketio.run(app, port=dev_port, host="0.0.0.0", debug=True)
